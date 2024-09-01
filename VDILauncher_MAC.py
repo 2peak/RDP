4a203de623857da79ee95deeb4731681ca330cb2
2peak <46584576+2peak@users.noreply.github.com>
2024-09-01 20:35:21 +0900

    Add files via upload
    MAC RDP Launcher

---
+++ VDILauncher_MAC.py
@@ -0,0 +1,50 @@
+from ping3 import ping
+import subprocess, time, os, os.path
+
+def ping_check(ip_address):
+    response = ping(ip_address)
+    if response is not None:
+        return True
+    else:
+        return False
+
+def CheckNetwork():
+   AD = "192.168.1.246"
+   response = ping_check(AD)
+   if response == True:
+       print("Internal Network Coneected. This Script will choose the RDP.")
+       RDPIF()
+   else:
+       print("It detected wasn't Internal Network. We'll Connect the VPN.")
+       VPN()
+
+def VPN():
+    file_response = os.path.isfile("/opt/homebrew/etc/wireguard/wg0.conf")
+    if file_response == False:
+        print("Wireguard Configuration File wasn't Founded. This Script Will be shutdown.")
+    else:
+        os.system('wg-quick up wg0')
+        time.sleep(15)
+        CheckNetwork()
+
+def RDPIF():
+    print("Choose the VDI Server")
+    print("1. LAB VM")
+    print("2. Finance VDI")
+    VDI = int(input("Your Answer : "))
+    if VDI == 1:
+        LAB()
+    if VDI == 2:
+        Finance()
+
+def LAB():
+    username = input("Enter the Username :")
+    password = input("Enter The Password. :")
+    os.system(f'xfreerdp /u:{username} /p:{password} /d:ayinfra.com /v:lab.external.com')
+    
+def Finance(): 
+    username = input("Enter the Username :")
+    password = input("Enter The Password. :")
+    os.system(f'xfreerdp /u:{username} /p:{password} /d:ayinfra.com /v:finance.external.com')
+    
+CheckNetwork()
