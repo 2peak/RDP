f4cf9ef76a29fd4ded6e1a99373655bc5319557e
Sangwon Yun <46584576+2peak@users.noreply.github.com>
2024-09-06 21:27:54 +0900

    Add files via upload
    VPN 연결해제 메뉴 추가 및 RDP 연결 해제시 RDPIF 함수로 자동호출 기능 추가함.

--- VDILauncher_MAC.py
+++ VDILauncher_MAC.py
@@ -9,7 +9,7 @@ def ping_check(ip_address):
         return False
 
 def CheckNetwork():
-   AD = "ad.ayinfra.com"
+   AD = "192.168.1.246"
    response = ping_check(AD)
    if response == True:
        print("Internal Network Coneected. This Script will choose the RDP.")
@@ -24,27 +24,44 @@ def VPN():
         print("Wireguard Configuration File wasn't Founded. This Script Will be shutdown.")
     else:
         os.system('wg-quick up wg0')
-        time.sleep(10)
+        time.sleep(15)
         CheckNetwork()
+
+def VPNDisconnect():
+    print("VPN will be Disconnected.")
+    os.system('wg-quick down wg0')
 
 def RDPIF():
     print("Choose the VDI Server")
     print("1. LAB VM")
     print("2. Finance VDI")
+    print("3. VPN Disconnect")
     VDI = int(input("Your Answer : "))
     if VDI == 1:
         LAB()
     if VDI == 2:
         Finance()
+    if VDI == 3:
+        VPNDisconnect()
+        
 
 def LAB():
     username = input("Enter the Username :")
     password = getpass.getpass("Enter The Password. :")
-    os.system(f'xfreerdp /u:{username} /p:{password} /d:ayinfra.com /v:lab.external.com')
+    process = os.system(f'xfreerdp /u:{username} /p:{password} /d:ayinfra.com /v:lab.external.com')
+    if process == 0:
+        RDPIF()
+    else:
+        print("RDP Error was occured. Please check the Error message.")
+    
     
 def Finance(): 
     username = input("Enter the Username :")
     password = getpass.getpass("Enter The Password. :")
-    os.system(f'xfreerdp /u:{username} /p:{password} /d:ayinfra.com /v:finance.external.com')
+    process = os.system(f'xfreerdp /u:{username} /p:{password} /d:ayinfra.com /v:finance.external.com')
+    if process == 0:
+        RDPIF()
+    else:
+        print("RDP Error was occured. Please check the Error message.")
     
 CheckNetwork()
